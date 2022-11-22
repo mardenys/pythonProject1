8427c016690e2794be2c779b4ec4c3a9ddd1ace2
Marcin Denysenko <marcin.denysenko@empirica.io>
2022-11-22 20:06:22 +0100

    guess the number

--- Day 4/guess_the_number.py
+++ Day 4/guess_the_number.py
@@ -1,29 +1,27 @@
 from random import randint
 print("Hey, what is your name?")
 name = input()
-print(f"Hi {name}, guess a number between 1 and 100 in 8 tries maximum.")
+print(f"Hi {name}, guess a number between 1 and 100 in 8 tries maximum.\nWhat number do you think it is?")
 
 guess_number = 0
 random_number = randint(1,100)
 #print(random_number)
-print("What number do you think it is?")
+
 while guess_number < 8:
     guess = int(input("Input number: "))
     guess_number = guess_number + 1
 
     if guess < random_number and guess > 0:
         print("too low")
-    if guess > random_number and guess <= 100:
+    elif guess > random_number and guess <= 100:
         print("too high")
-    if guess <= 0:
+    elif guess <= 0:
         print("number must be higher than 0")
-    if guess > 100:
+    elif guess > 100:
         print("Number must be lower than 101")
-    if guess == random_number:
+    else:
+        print(f'Well done {name}, {guess} is the correct number. Number of tries to guess it: {guess_number}')
         break
-
-if guess == random_number:
-    print(f'Well done {name}, {guess} is the correct number. Number of tries to guess it: {guess_number}')
 
 if guess != random_number:
     print(f"Sorry {name}, you run out of tries. Correct number was {random_number}")
